9ae51043907272193c8b3b597c6541904a08a6a9
sahaja <sahajaboyapati@gmail.com>
2023-11-09 14:39:03 +0530

    Intial commit

---
+++ ConDataTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                string name = "Boyapati Sahaja"; 
+                int age = 23; 
+                double height = 150.2; 
+
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age: " + age);
+                Console.WriteLine("Height (in cm): " + height);
+            Console.ReadKey();
+                // Console.WriteLine($"Name: {name}");
+                // Console.WriteLine($"Age: {age}");
+                // Console.WriteLine($"Height (in cm): {height}");
+            }
+        }
+
+    }
+
+
